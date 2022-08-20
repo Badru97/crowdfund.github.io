c71dac8e8b1af1aac96749dcbf9c2adeae49b58d
Badru97 <danielduskuw@gmail.com>
2022-08-20 11:44:48 +0100

    nwewLife

---
+++ crowdfd.js
@@ -0,0 +1,20 @@
+let changeState = document.getElementById('bookmarkbtn');
+
+changeState.addEventListener('click', function(){
+  changeState.textContent="Bookmarked";
+  changeState.style.color="#fff";
+  changeState.style.backgroundColor="hsl(176, 72%, 28%)";
+})
+
+/*let activeRadio = document.getElementById('radio');
+
+activeRadio.addEventListener('click', function(){
+  activeRadio.style.color="hsl(176, 72%, 28%)";
+})
+
+
+let rewardButton = document.getElementById('reawardBtn');
+
+rewardButton.addEventListener('click',function(){
+  rewardButton.classList.toggle
+})*/
